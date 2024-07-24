50be6707982fc57a2ff97a1384558a3184b8b757
RofaBR <rostislavbrigine@gmail.com>
2024-07-24 18:56:45 +0300

    error correction

--- migrations/initial_migration.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE links (
-    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-    original TEXT NOT NULL UNIQUE,
-    shortened TEXT NOT NULL UNIQUE,
-    created_at TIMESTAMP DEFAULT now()
-);
