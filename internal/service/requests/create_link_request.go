e78a06ca9ba4ec2d649ebec6d9ffd12b49dd5a69
RofaBR <rostislavbrigine@gmail.com>
2024-08-09 13:47:48 +0300

    update validate func

--- internal/service/requests/create_link_request.go
+++ internal/service/requests/create_link_request.go
@@ -1,6 +1,9 @@
 package requests
 
-import "errors"
+import (
+	"errors"
+	"net/url"
+)
 
 type CreateLinkRequest struct {
 	OriginalURL string `json:"original_url"`
@@ -10,5 +13,11 @@ func (r CreateLinkRequest) Validate() error {
 	if r.OriginalURL == "" {
 		return errors.New("original_url is required")
 	}
+
+	parsedURL, err := url.ParseRequestURI(r.OriginalURL)
+	if err != nil || parsedURL.Scheme == "" || parsedURL.Host == "" {
+		return errors.New("invalid original_url format")
+	}
+
 	return nil
 }
